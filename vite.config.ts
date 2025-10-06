24b8d4a3410455ed055d6e89504b685a4a697cd6
JunHyeong <ssong99999@naver.com>
2025-10-06 22:16:24 +0900

    env 환경변수에 따른 실행

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    envDir: 'env', // .env 파일들이 위치할 디렉토리
+    envPrefix: 'VITE_', // VITE_로 시작하는 환경변수만 클라이언트에 노출
+    server: {
+      port: 3000,
+      open: true,
+    },
+    build: {
+      outDir: 'dist',
+      sourcemap: mode === 'development',
+    },
+  }
 })
